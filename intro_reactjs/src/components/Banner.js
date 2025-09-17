5e4e0f884d94b32d7d5559f67ae7362457b3311f
BanlyTong <banlytong@gmail.com>
2025-09-17 10:21:27 +0700

    feat: intro to reactjs 2

---
+++ intro_reactjs/src/components/Banner.js
@@ -0,0 +1,7 @@
+export default function Banner() {
+  return (
+    <div className="bg-green-500 text-white p-4">
+      <h2 className="text-xl font-semibold">Welcome to the Banner Component</h2>
+    </div>
+  );
+}
---
+++ intro_reactjs/src/components/Categories.js
@@ -0,0 +1,7 @@
+export default function Categories() {
+  return (
+    <div className="bg-red-500 text-xl">
+      <h1>Hello Categories</h1>
+    </div>
+  );
+}
