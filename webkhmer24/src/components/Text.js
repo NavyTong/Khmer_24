284133b5c7522ee630163a9523d8e126ed924b84
NavyTong <navytongkh@gmail.com>
2025-10-12 16:07:24 +0700

    categorylast

--- webkhmer24/src/components/Text.js
+++ webkhmer24/src/components/Text.js
@@ -1,7 +1,9 @@
 export default function Text() {
   return (
-    <div className="ml-40 text-xl mt-5">
-      <h1 className="font-bold text-2xl">Welcom to khmer24.com, the biggest online market in Cambodia.</h1>
+    <div className="ml-47 text-xl mt-5">
+      <h1 className="font-bold text-2xl">
+        Welcom to khmer24.com, the biggest online market in Cambodia.
+      </h1>
     </div>
   );
 }
