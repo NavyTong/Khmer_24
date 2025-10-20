26a4e05e70e711611281d7c604179b51c829bd7c
NavyTong <navytongkh@gmail.com>
2025-10-20 10:24:16 +0700

    to do card

---
+++ webkhmer24/src/cards/Logocar.jsx
@@ -0,0 +1,18 @@
+export default function Logocar() {
+  return (
+    <div>
+      <div className="w-full ">
+        <img
+          className="mx-auto mt-10"
+          src="https://www.khmer24.ws/www/delivery/ai.php?filename=car(970x250).jpg%20(7)&contenttype=jpeg"
+          alt="logocar"
+        />
+      </div>
+      <div>
+        <button className="text-sky-600 mt-4 ml-50">
+          Home / Car and Vehicles / Cars for Sale / Cadillac / Escalade
+        </button>
+      </div>
+    </div>
+  );
+}
